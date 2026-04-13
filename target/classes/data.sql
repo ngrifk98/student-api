d95ef7e01a4ce00ec6519da0a9eccf14b8dbbf8a
ngrifk98 <ngrifk@gmail.com>
2026-04-12 21:18:29 -0500

    Add .gitignore, remove build artifacts from tracking

--- target/classes/data.sql
+++
@@ -1,7 +0,0 @@
--- This file auto-runs on startup because Spring Boot detects it in resources/
--- Seeds the H2 in-memory database with sample student data
-
-INSERT INTO students (first_name, last_name, email, major) VALUES ('Nick', 'Kingston', 'nick@classboxes.com', 'Computer Science');
-INSERT INTO students (first_name, last_name, email, major) VALUES ('Surya', 'Patel', 'surya@classboxes.com', 'Information Systems');
-INSERT INTO students (first_name, last_name, email, major) VALUES ('Arj', 'Kumar', 'arj@classboxes.com', 'Software Engineering');
-INSERT INTO students (first_name, last_name, email, major) VALUES ('Kunal', 'Kabi', 'kunal@classboxes.com', 'Computer Science');
